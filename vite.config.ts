d6b3e622beb1cbf9cde3b3179b0439cd98e38feb
yaffalhakim1 <yafialhakim64@gmail.com>
2025-10-04 12:11:35 +0700

    feat: add Netlify plugin for React Router and configuration files

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,17 @@
-import { reactRouter } from "@react-router/dev/vite";
-import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
-import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
+import { reactRouter } from '@react-router/dev/vite';
+import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
+import netlifyPlugin from '@netlify/vite-plugin-react-router';
 
 export default defineConfig({
   plugins: [
     tailwindcss(),
     reactRouter(),
     tsconfigPaths(),
+    netlifyPlugin(),
+
     ViteImageOptimizer({
       // Default optimization settings
       png: {
@@ -28,11 +31,9 @@ export default defineConfig({
         plugins: [
           {
             name: 'removeViewBox',
-            active: false,
           },
           {
             name: 'removeEmptyAttrs',
-            active: false,
           },
         ],
       },
